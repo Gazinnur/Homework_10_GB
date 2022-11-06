72d8e6bd32e8208dab6629d75d88742b9fc4abc5
gazinnur <gazinnur@mail.ru>
2022-11-06 22:20:06 +0300

    Решение задачи №27 нахождение суммы цифр числа

---
+++ Exp002_SumOFnum/Program.cs
@@ -0,0 +1,44 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+//Console.WriteLine("Введите число");
+//string Num = Console.ReadLine();
+//Console.WriteLine(Num);
+//int sum = 0;
+//int length = Num.Length;
+
+//for (int i = 0; i < length; i++)
+//{
+//   int g=Convert.ToInt32(Num[i]);
+//g = Num[i];
+// sum = sum + g;
+//Console.WriteLine(g);
+// Console.WriteLine(Num[1]);
+//}
+//Console.WriteLine(length);
+
+//int SumOf(string n)
+//{
+//  int sum = 0;
+//int length = n.Length;
+//for (int i = 0; i < length; i++)
+//{
+//  n[i]=Convert.ToInt32(n[i]);
+//sum = sum + n[i];
+//}
+//return sum;
+//}
+
+//int result = SumOf(Num);
+//Console.WriteLine(result);
+
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if (Num < 0) Num = -Num;
+for (; Num % 10 != 0; Num = Num / 10)
+{
+    sum = sum + Num % 10;
+}
+Console.WriteLine(sum);
